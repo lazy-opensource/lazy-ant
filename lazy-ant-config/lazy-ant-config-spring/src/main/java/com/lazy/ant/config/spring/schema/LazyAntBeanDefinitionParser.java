d98e6c8229fcdef97250b9ef47c236dad34ff8c6
lzy369 <1059446341@qq.com>
2018-07-08 23:19:23 +0800

    初始化项目完成

--- lazy-ant-config/lazy-ant-config-spring/src/main/java/com/lazy/ant/config/spring/schema/LazyAntBeanDefinitionParser.java
+++ lazy-ant-config/lazy-ant-config-spring/src/main/java/com/lazy/ant/config/spring/schema/LazyAntBeanDefinitionParser.java
@@ -1,10 +1,21 @@
 package com.lazy.ant.config.spring.schema;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.xml.BeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.w3c.dom.Element;
+
 /**
  * <p></p>
  *
  * @author laizhiyuan
  * @date 2018/6/29.
  */
-public class LazyAntBeanDefinitionParser {
+public class LazyAntBeanDefinitionParser implements BeanDefinitionParser {
+
+
+    @Override
+    public BeanDefinition parse(Element element, ParserContext parserContext) {
+        return null;
+    }
 }
--- lazy-ant-config/lazy-ant-config-spring/src/main/java/com/lazy/ant/config/spring/schema/LazyAntNamespaceHandler.java
+++ lazy-ant-config/lazy-ant-config-spring/src/main/java/com/lazy/ant/config/spring/schema/LazyAntNamespaceHandler.java
@@ -1,10 +1,18 @@
 package com.lazy.ant.config.spring.schema;
 
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
 /**
  * <p></p>
  *
  * @author laizhiyuan
  * @date 2018/6/29.
  */
-public class LazyAntNamespaceHandler {
+public class LazyAntNamespaceHandler extends NamespaceHandlerSupport {
+
+
+    @Override
+    public void init() {
+
+    }
 }
