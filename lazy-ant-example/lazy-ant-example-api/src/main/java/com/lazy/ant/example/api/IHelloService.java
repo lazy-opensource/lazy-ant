0b205192fa015bacdfc24204d8572d50ca2cc546
lzy369 <1059446341@qq.com>
2018-07-08 22:48:38 +0800

    初始化项目完成

---
+++ lazy-ant-example/lazy-ant-example-api/src/main/java/com/lazy/ant/example/api/IHelloService.java
@@ -0,0 +1,14 @@
+package com.lazy.ant.example.api;
+
+/**
+ * <p>
+ *     Lazy Anit Example
+ * </p>
+ *
+ * @author laizhiyuan
+ * @date 2018/7/7.
+ */
+public interface IHelloService {
+
+    void sayHello(String name);
+}
